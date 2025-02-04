a537040e720160fafda88f3e0ec1ad25b8b02d48
Allan Bispo <allandellon.desbra@gmail.com>
2025-02-03 22:12:26 -0300

    Criação do efeito carousel inicial

--- src/js/home.js
+++ src/js/home.js
@@ -2,44 +2,70 @@ const keepWatchingMediaCard = [
     {title:'Naruto', linkImg:'naruto_classic.jpg'},
     {title:'The chosen', linkImg:'the_chosen.jpg'},
     {title:'Os Pinguins de Madagascar', linkImg:'os_pinguins.jpg'},
-    {title:'Anne Whith An E', linkImg:'anne_with_an_e.jpg'},
+    {title:'Anne With An E', linkImg:'anne_with_an_e.jpg'},
     {title:'Madagascar 2', linkImg:'madagascar2.jpg'},
-    {title:'Lá casa de papel', linkImg:'la_casa_de_papel.jpg'},
+    {title:'La Casa de Papel', linkImg:'la_casa_de_papel.jpg'},
+    {title:'Angry Birds', linkImg:'angry_birds.png'},
+    {title:'Naruto', linkImg:'naruto_classic.jpg'},
+    {title:'The chosen', linkImg:'the_chosen.jpg'},
+    {title:'Os Pinguins de Madagascar', linkImg:'os_pinguins.jpg'},
+    {title:'Anne With An E', linkImg:'anne_with_an_e.jpg'},
+    {title:'Madagascar 2', linkImg:'madagascar2.jpg'},
+    {title:'La Casa de Papel', linkImg:'la_casa_de_papel.jpg'},
+    {title:'Angry Birds', linkImg:'angry_birds.png'},
+    {title:'The chosen', linkImg:'the_chosen.jpg'},
+    {title:'Os Pinguins de Madagascar', linkImg:'os_pinguins.jpg'},
+    {title:'Anne With An E', linkImg:'anne_with_an_e.jpg'},
+    {title:'Madagascar 2', linkImg:'madagascar2.jpg'},
+    {title:'La Casa de Papel', linkImg:'la_casa_de_papel.jpg'},
     {title:'Angry Birds', linkImg:'angry_birds.png'}
-]
+];
 
-function createKeepWatching(){
-
+function createKeepWatching() {
     let section = document.getElementById('keepWatchingMediaCard');
+    section.style.transition = 'transform 0.3s ease-in-out';
 
     keepWatchingMediaCard.forEach((e) => {
         let div = document.createElement('div');
-        div.classList.add('mediaCard');
-
-        div.style.backgroundImage = `url(../assets/img/home/keepWatching/${e.linkImg})`
-
+        div.classList.add('mediaCard', 'position-relative');
+        div.style.backgroundImage = `url(../assets/img/home/keepWatching/${e.linkImg})`;
         section.append(div);
     });
+
 }
 
+// Chama a função para criar os elementos
 createKeepWatching();
 
+    // Adiciona os eventos aos botões após a criação dos elementos
+    const prevBtn = document.getElementById("prevBtn");
+    const nextBtn = document.getElementById("nextBtn");
+    let position = 0; // Largura do card + gap
+    let count = 0;
 
+nextBtn.addEventListener("click", () => {
+    prevBtn.style.display = 'block';
 
-{/* <div class="d-flex gap-4">
-    <div class="mediaCard "></div>
-</div> */}
+    if (position !== 0) {
+        position += 400;
+        let section = document.getElementById('keepWatchingMediaCard');
+        section.style.transform = `translateX(${position}px)`;
+        count--;
+        position == 0 ? nextBtn.style.display = 'none' : '';
+    }
+});
 
+prevBtn.addEventListener("click", () => {
+    nextBtn.style.display = 'block';
 
-
-// .mediaCard{
-//     width: 350px;
-//     height: 200px;
-//     border-radius: 2%;
-//     background-image: url(../../assets/img/home/keepWatching/naruto_classic.jpg);
-//     background-size: cover;
-// }
-
-
-
+    let qtdCardsTela = (screen.width/380)+2;
+    if (count < (keepWatchingMediaCard.length - qtdCardsTela)) {
+        let section = document.getElementById('keepWatchingMediaCard');
+        position -= 400;
+        section.style.transform = `translateX(${position}px)`;      
+        count++;
+        count > (keepWatchingMediaCard.length - qtdCardsTela) ? prevBtn.style.display = 'none': '';
+    }
+        
+});
 
